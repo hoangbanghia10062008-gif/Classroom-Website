52dabebd2ccdd1e938806f098b7146767249997b
Nghia <hoangbanghia10062008@gmail.com>
2025-11-05 01:43:46 -0500

    push

--- phonics/phonics.js
+++ phonics/phonics.js
@@ -1,15 +1,16 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-	coll[i].addEventListener("click", function() {
-		this.classList.toggle("collapsible-active");
-		var content = this.nextElementSibling;
-		if (content.style.maxHeight){
-			content.style.maxHeight = null;
-		}
-		else {
-			content.style.maxHeight = content.scrollHeight + "px";
-		} 
-	});
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const coll = document.getElementsByClassName("collapsible");
+    
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
+            
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+});
