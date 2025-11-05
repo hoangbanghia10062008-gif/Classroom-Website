b4aceb7023a6c9e916aea2b0b48ecb055e87eaba
Nghia <hoangbanghia10062008@gmail.com>
2025-11-05 01:47:31 -0500

    push

--- phonics/phonics.js
+++ phonics/phonics.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
     for (let i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function() {
-            this.classList.toggle("active");
+            this.classList.toggle("collapsible-active");
             const content = this.nextElementSibling;
             
             if (content.style.maxHeight) {
